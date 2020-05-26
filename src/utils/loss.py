c5b5d996fd59ba04ca31fd88985b7d6d5a896cea
Alex Fauduet <alex.fauduet@eleves.enpc.fr>
2020-05-26 14:07:31 +0200

    other classifiers

--- src/utils/loss.py
+++ src/utils/loss.py
@@ -3,3 +3,11 @@ def jaccard(str1, str2):
     b = set(str2.lower().split())
     c = a.intersection(b)
     return float(len(c)) / (len(a) + len(b) - len(c))
+
+
+def mean_jaccard(ground_truths, predicted_strings):
+    avg = 0
+    for i in range(len(predicted_strings)):
+        avg += jaccard(predicted_strings[i], ground_truths[i])
+
+    return avg
